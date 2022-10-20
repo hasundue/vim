1cc668edf97df5d08685cf143b964cac8ebf5b09
hasundue <hasundue@gmail.com>
2022-10-21 08:44:37 +0900

    open terminal when nvim is executed without an initial file

--- init.nvim.lua
+++ init.nvim.lua
@@ -6,17 +6,30 @@ vim.go.winblend = 10
 
 -- }
 
--- 
+--
 -- Terminal {
--- 
+--
 vim.api.nvim_set_keymap("t", "<Esc>", "<C-\\><C-n>", {})
 
-vim.api.nvim_create_autocmd({"TermOpen"}, {
-  pattern = {"*"},
+local config_term = function()
+  vim.opt_local.termguicolors = true
+  vim.opt_local.number = false
+  vim.opt_local.relativenumber = false
+end
+
+vim.api.nvim_create_autocmd({ "TermOpen" }, {
+  pattern = { "*" },
+  callback = config_term,
+})
+
+-- open a terminal if nvim is executed without an argument
+vim.api.nvim_create_autocmd({ "VimEnter" }, {
   callback = function()
-    vim.opt_local.termguicolors = true
-    vim.opt_local.number = false
-    vim.opt_local.relativenumber = false
+    local bufname = vim.api.nvim_buf_get_name(0)
+    if (bufname == "") then
+      vim.api.nvim_command("term")
+      config_term()
+    end
   end
 })
 
