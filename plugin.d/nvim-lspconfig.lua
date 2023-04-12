d42a02cff807d8c2003ef54edc65e49d1e280959
hasundue <hasundue@gmail.com>
2023-04-12 18:20:14 +0900

    nvim-lspconfig: add rust-analyzer and deno.test

--- plugin.d/nvim-lspconfig.lua
+++ plugin.d/nvim-lspconfig.lua
@@ -20,7 +20,6 @@ local function peek_definition()
   return vim.lsp.buf_request(0, 'textDocument/definition', params, preview_location_callback)
 end
 
-
 vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('UserLspConfig', {}),
   callback = function(ev)
@@ -128,6 +127,16 @@ local servers = {
 
 for server, config in pairs(servers) do
   lspconfig[server].setup(config)
+end
+
+vim.lsp.commands["deno.test"] = function(args)
+  local file = args.arguments[1]:gsub("file:///", "/")
+  local test_name = "/^" .. args.arguments[2] .. "$/"
+  local maybe_debug = args.title == "Debug" and "--inspect-brk " or ""
+
+  local cmd = "split term://deno test --no-check --unstable -A " ..
+      maybe_debug .. file .. " --filter " .. vim.fn.fnameescape(test_name)
+  vim.cmd(cmd)
 end
 
 -- }}}
