199b4e7723fab44a502d56845668f836ecf62be8
hasundue <hasundue@gmail.com>
2023-09-04 13:46:47 +0900

    terminal: disable autocmds

--- plugin/terminal.lua
+++ plugin/terminal.lua
@@ -16,30 +16,30 @@ vim.api.nvim_create_autocmd({ "TermOpen" }, {
 })
 
 -- open a terminal if nvim is executed without an argument
-vim.api.nvim_create_autocmd({ "VimEnter" }, {
-  callback = function()
-    local bufname = vim.api.nvim_buf_get_name(0)
-    if (bufname == "") then
-      vim.api.nvim_command("term")
-      config_term()
-    end
-  end
-})
+-- vim.api.nvim_create_autocmd({ "VimEnter" }, {
+--   callback = function()
+--     local bufname = vim.api.nvim_buf_get_name(0)
+--     if (bufname == "") then
+--       vim.api.nvim_command("term")
+--       config_term()
+--     end
+--   end
+-- })
 
 -- update the current working directory accordingly
-vim.api.nvim_create_autocmd({ "TermLeave" }, {
-  callback = function()
-    local bufnr = vim.api.nvim_get_current_buf()
-    local buftype = vim.api.nvim_buf_get_option(bufnr, 'buftype')
-    if buftype == 'terminal' then
-      local line = vim.api.nvim_get_current_line()
-      local dir = line:match('.*:(.-)%$.*')
-      if dir then
-        dir = vim.fn.expand(dir)
-        vim.api.nvim_command('lcd ' .. vim.fn.fnameescape(dir))
-      end
-    end
-  end
-})
+-- vim.api.nvim_create_autocmd({ "TermLeave" }, {
+--   callback = function()
+--     local bufnr = vim.api.nvim_get_current_buf()
+--     local buftype = vim.api.nvim_buf_get_option(bufnr, 'buftype')
+--     if buftype == 'terminal' then
+--       local line = vim.api.nvim_get_current_line()
+--       local dir = line:match('.*:(.-)%$.*')
+--       if dir then
+--         dir = vim.fn.expand(dir)
+--         vim.api.nvim_command('lcd ' .. vim.fn.fnameescape(dir))
+--       end
+--     end
+--   end
+-- })
 
 -- }
